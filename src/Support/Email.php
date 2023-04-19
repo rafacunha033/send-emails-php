bc0b0b4fe3aaa137c91aaf308dc4746878d8223b
rafacunha033 <rafacunha.prof033@gmail.com>
2023-04-19 17:00:06 -0300

    Email core functions

--- src/Support/Email.php
+++ src/Support/Email.php
@@ -35,4 +35,45 @@ class Email
         $this->mail->Username = MAIL["username"];
         $this->mail->Password = MAIL["password"];
     }
+
+    public function add(string $subject, string $body, string $recipient_name, string $recipient_email): Email 
+    {
+        $this->data->subject = $subject;
+        $this->data->body = $body;
+        $this->data->recipient_name = $recipient_name;
+        $this->data->recipient_email = $recipient_email;
+        return $this; 
+    }
+
+    public function attach(string $filePath, string $fileName): Email
+    {
+        $this->data->attach[$filePath] = $fileName;
+    }
+
+    public function send(string $from_name = MAIL["from_name"], string $from_email = MAIL["from_email"]): bool
+    {
+        try{
+            $this->mail->Subject = $this->data->subject;
+            $this->mail->msgHTML($this->data->body);
+            $this->mail->addAddress($this->data->recipient_email, $this->data->recipient_name);
+            $this->mail->setFrom($from_email, $from_name);
+
+            if(!empty($this->data->attach)) {
+                foreach ($this->data->attach as $path => $name) {
+                    $this->mail->addAttachment($path, $name);
+                }
+            }
+
+            $this->mail->send();
+            return true;
+        } catch (Exception $exception) {
+            $this->error = $exception;
+            return false;
+        }     
+    }
+
+    public function error(): ?Exception
+    {
+        return $this->error;
+    }
 }
